61f8c442cfd69d232a771e787b996a05e68e2c9d
divyasaipa <divyasaipa>
2024-01-09 00:16:09 +0000

    Initial commit

---
+++ src/main/java/com/myfirstapplication/Application.java
@@ -0,0 +1,10 @@
+package com.myfirstapplication;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/myfirstapplication/MyfirstapplicationTest.java
@@ -0,0 +1,21 @@
+package com.myfirstapplication;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MyfirstapplicationTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
